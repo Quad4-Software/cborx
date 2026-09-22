3c0e5d252e204368f917f423b989d5b0f4163eaf
Ivan <ivan@quad4.io>
2026-09-22 09:44:10 -0500

    chore: bump to 0.1.1 after release workflow fix

--- src/cborx/__init__.py
+++ src/cborx/__init__.py
@@ -6,7 +6,7 @@ from .encoder import CBOREncoder, DefaultCallback, dump, dumps
 from .exceptions import CBORDecodeError, CBOREncodeError, CBORError
 from .types import CBORSimpleValue, CBORTag, UndefinedType, undefined
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "CBORDecodeError",
