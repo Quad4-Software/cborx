3a8b1e1166b964aeea275889ec7aae84c02179d9
Ivan <ivan@quad4.io>
2026-09-23 04:06:24 -0500

    chore(release): 0.2.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Changelog
 
-## Unreleased
+## [0.2.4] - 2026-09-23
+
+Fix tag 1 datetime decoding on Windows.
 
 - Tag 1 decoding no longer calls datetime.fromtimestamp, whose C
   library call rejects negative timestamps on Windows. Pre-epoch
--- src/cborx/__init__.py
+++ src/cborx/__init__.py
@@ -6,7 +6,7 @@ from .encoder import CBOREncoder, DefaultCallback, dump, dumps
 from .exceptions import CBORDecodeError, CBOREncodeError, CBORError
 from .types import CBORSimpleValue, CBORTag, UndefinedType, undefined
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 __all__ = [
     "CBORDecodeError",
